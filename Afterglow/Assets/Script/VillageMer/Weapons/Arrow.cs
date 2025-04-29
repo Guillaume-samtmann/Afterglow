dea173c329a627b49d8a7840710d58aa8346f76b
guillaume <guillaumesamtmann24@gmail.com>
2025-04-29 08:12:47 +0200

    arc + tir fleche

---
+++ Afterglow/Assets/Script/VillageMer/Weapons/Arrow.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Arrow : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int arrowDamage = 25;
+    private bool isStuck = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Destroy(gameObject, 50f);
+
+        Collider arrowCollider = GetComponent<Collider>();
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            Collider[] playerColliders = player.GetComponentsInChildren<Collider>();
+            foreach (Collider playerCollider in playerColliders)
+            {
+                Physics.IgnoreCollision(arrowCollider, playerCollider);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!isStuck && !collision.transform.CompareTag("Player"))
+        {
+            isStuck = true;
+
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+
+            rb.isKinematic = true;
+
+            Debug.Log("Arrow stuck :" + collision.transform.name);
+        }
+
+        /*if (collision.transform.GetComponent<Animal>())
+        {
+            Animal animal = collision.transform.GetComponent<Animal>();
+            animal.TakeDamage(arrowDamage);
+        }*/
+    }
+}
