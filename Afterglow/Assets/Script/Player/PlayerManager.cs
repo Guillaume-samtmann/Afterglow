b08b49d2822c007feffec508643bd4816f20996f
guillaume <guillaumesamtmann24@gmail.com>
2025-05-07 08:06:06 +0200

    switch tool & Ai

--- Afterglow/Assets/Script/Player/PlayerManager.cs
+++ Afterglow/Assets/Script/Player/PlayerManager.cs
@@ -6,17 +6,50 @@ public class PlayerManager : MonoBehaviour
 {
     public int playerLife = 20;
 
+    [Header("tools")]
+    public GameObject arc;
+    public GameObject cannePeche;
+    public GameObject pioche;
+
 
     private void Update()
     {
         Debug.Log(playerLife);
         dead();
+        switchTools();
     }
 
     public void dead()
     {
         if(playerLife <= 0) {
             Debug.Log("perso mort");
+        }
+    }
+
+    public void switchTools()
+    {
+        if(Input.GetKeyUp(KeyCode.Alpha1)) {
+            arc.SetActive(false);
+            cannePeche.SetActive(false);
+            pioche.SetActive(false);
+        }
+        if(Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            arc.SetActive(true);
+            cannePeche.SetActive(false);
+            pioche.SetActive(false);
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            arc.SetActive(false);
+            cannePeche.SetActive(true);
+            pioche.SetActive(false);
+        }
+        if(Input.GetKeyUp(KeyCode.Alpha4))
+        {
+            arc.SetActive(false);
+            cannePeche.SetActive(false);
+            pioche.SetActive(true);
         }
     }
 }
