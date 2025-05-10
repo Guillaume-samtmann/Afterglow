28eda3294c01f395975326e99c6db1a5ff0711d4
guillaume <guillaumesamtmann24@gmail.com>
2025-05-10 21:40:01 +0200

    add portal

--- Afterglow/Assets/Script/ChampiVille/camRayCastChampi.cs
+++ Afterglow/Assets/Script/ChampiVille/camRayCastChampi.cs
@@ -12,19 +12,45 @@ public class camRayCastChampi : MonoBehaviour
     public GameObject btnE_food;
     public GameObject btnE;
     public GameObject btnE_money;
+    public GameObject btnE_arc;
 
     [Header("Items")]
     public GameObject bag;
     public GameObject money;
-    public GameObject food;
+    //public GameObject food;
+
+    public GameObject carrot;
+    public GameObject bottle;
+    public GameObject potato;
+    public GameObject loaf;
+    public GameObject meat;
+
+    public GameObject arc;
+
+
+    public GameObject task1;
+    public GameObject task2;
+    public GameObject task3;
 
     public bool canPickUpBag = false;
     public bool canPickUpMoney = false;
-    public bool canPickUpFood = false;
+    //public bool canPickUpFood = false;
+    public bool canPickUpCarrot = false;
+    public bool canPickUpBottle = false;
+    public bool canPickUpPotato = false;
+    public bool canPickUpLoaf = false;
+    public bool canPickUpMeat = false;
+    public bool canPickUpArc = false;
 
     public bool isPickUpBag = false;
     public bool isPickUpMoney = false;
-    public bool isPickUpFood = false;
+    //public bool isPickUpFood = false;
+    public bool isPickUpCarrot = false;
+    public bool isPickUpBottle = false;
+    public bool isPickUpPotato = false;
+    public bool isPickUpLoaf = false;
+    public bool isPickUpMeat = false;
+    public bool isPickUpArc = false;
     private void HighlightObject(GameObject obj, bool highlight)
     {
         Outline outiline = obj.GetComponent<Outline>();
@@ -59,22 +85,60 @@ public class camRayCastChampi : MonoBehaviour
 
             if (hittedObject.CompareTag("food"))
             {
+                if(hittedObject.name == "carrot (1)")
+                {
+                    HighlightObject(hittedObject, true);
+                    btnE_food.SetActive(true);
+                    canPickUpCarrot = true;
+                }
+                if (hittedObject.name == "wicker_bottle")
+                {
+                    HighlightObject(hittedObject, true);
+                    btnE_food.SetActive(true);
+                    canPickUpBottle = true;
+                }
+                if (hittedObject.name == "big_meat")
+                {
+                    HighlightObject(hittedObject, true);
+                    btnE_food.SetActive(true);
+                    canPickUpMeat = true;
+                }
+                if (hittedObject.name == "potato (3)")
+                {
+                    HighlightObject(hittedObject, true);
+                    btnE_food.SetActive(true);
+                    canPickUpPotato = true;
+                }
+                if (hittedObject.name == "long_loaf")
+                {
+                    HighlightObject(hittedObject, true);
+                    btnE_food.SetActive(true);
+                    canPickUpLoaf = true;
+                }
+            }
+            if (hittedObject.CompareTag("arc"))
+            {
                 HighlightObject(hittedObject, true);
-                btnE_food.SetActive(true);
-                canPickUpFood = true;
+                btnE_arc.SetActive(true);
+                canPickUpArc = true;
             }
         }
         else
         {
             hittedObject = null;
             canPickUpBag = false;
-            canPickUpFood = false;
+            //canPickUpFood = false;
+            canPickUpCarrot = false;
             canPickUpMoney = false;
+            canPickUpBottle =false;
+            canPickUpMeat = false;
+            canPickUpLoaf = false;
 
             btnE.SetActive(false);
             btnE_food.SetActive(false);
             btnE_money.SetActive(false);
             btnE_bag.SetActive(false);
+            btnE_arc.SetActive(false);
         }
 
         if (previousHittedObject != hittedObject)
@@ -94,6 +158,7 @@ public class camRayCastChampi : MonoBehaviour
             bag.SetActive(false);
             isPickUpBag = true;
             btnE.SetActive(false);
+            task1.SetActive(true);
         }
 
         if (Input.GetKeyUp(KeyCode.E) && canPickUpMoney)
@@ -101,13 +166,44 @@ public class camRayCastChampi : MonoBehaviour
             money.SetActive(false);
             isPickUpMoney = true;
             btnE.SetActive(false);
+            task2.SetActive(true);
         }
 
-        if (Input.GetKeyUp(KeyCode.E) && canPickUpFood)
+        if (Input.GetKeyUp(KeyCode.E) && canPickUpCarrot)
         {
-            food.SetActive(false);
-            isPickUpFood = true;
+            carrot.SetActive(false);
+            isPickUpCarrot = true;
             btnE.SetActive(false);
+        }
+        if (Input.GetKeyUp(KeyCode.E) && canPickUpBottle)
+        {
+            bottle.SetActive(false);
+            isPickUpBottle = true;
+            btnE.SetActive(false);
+        }
+        if (Input.GetKeyUp(KeyCode.E) && canPickUpMeat)
+        {
+            meat.SetActive(false);
+            isPickUpMeat = true;
+        }
+        if (Input.GetKeyUp(KeyCode.E) && canPickUpPotato)
+        {
+            potato.SetActive(false);
+            isPickUpPotato = true;
+        }
+        if (Input.GetKeyUp(KeyCode.E) && canPickUpLoaf)
+        {
+            loaf.SetActive(false);
+            isPickUpLoaf = true;
+        }
+        if(isPickUpCarrot && isPickUpBottle && isPickUpMeat && isPickUpPotato && isPickUpLoaf)
+        {
+            task3.SetActive(true);
+        }
+        if(Input.GetKey(KeyCode.E) && canPickUpArc)
+        {
+            arc.SetActive(false);
+            isPickUpArc = true;
         }
     }
 
